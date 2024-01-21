f6d19c88d99779b784c3329f0329695925480d12
Shiwantha-I-Rodrigo <shiwantha.indunil@gmail.com>
2024-01-21 22:14:12 +0100

    hello

---
+++ hello.tcl
@@ -0,0 +1,7 @@
+#!/usr/bin/tclsh
+puts "Hello, tcl world!"
+exit 0
+
+# sudo pacman -S tcl
+# Extensions > ... > install from vsix > choose (tcl-0.4.3.vsix) for tcl syntax highlight extention
+# tclsh hello.tcl
